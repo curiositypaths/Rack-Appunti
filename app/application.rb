c3298cb89fe1d0a42d3703b227bb84f8067b3518
curiositypaths <curiositypaths@users.noreply.github.com>
2016-12-30 13:37:17 -0500

    Add config.ru and enviroment.rb

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,5 @@
-require 'rack'
-require 'rack/server'
-
 class AppuntiApp
   def self.call(env)
     [ 200, { }, ['Welcome to your notebook'] ]
   end
 end
-
-Rack::Server.start :app => AppuntiApp
---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+require 'rack'
+require 'rack/server'
+
+require_relative '../app/application'
