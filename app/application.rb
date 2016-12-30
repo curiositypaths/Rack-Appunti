af0776167f781e2d7c5901fd7a890187c4183d4f
curiositypaths <curiositypaths@users.noreply.github.com>
2016-12-30 15:35:48 -0500

    Add page layout functionality

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,36 @@
 class AppuntiApp
 
+  # Layout rendering methods
+  def prepare_page_layout
+    layout_path = File.expand_path("app/views/layout.html.erb")
+    ERB.new(File.read(layout_path)).result(binding)
+  end
+
+  def prepare_action_layout(template)
+    template_path = File.expand_path("app/views/#{template}.html.erb")
+    action_layout = ERB.new(File.read(template_path)).result(binding)
+    prepare_page_layout {action_layout}
+  end
+
+  def render(template)
+    @response.write prepare_action_layout template
+    @response.finish
+  end
+
+  # Rack response handling methods
   def initialize_request_and_response(env)
     @request = Rack::Request.new env
     @response = Rack::Response.new
   end
 
-  def erb(template)
-    path = File.expand_path("#{template}")
-    ERB.new(File.read(path)).result(binding)
-  end
-
-  def render(template)
-    @response.write erb template
-    @response.finish
-  end
-
   def call(env)
     initialize_request_and_response(env)
-    @app_name = 'notebook'
-    render "index.html.erb"
+    route_response
+  end
+
+  # Application logic methods
+  def route_response
+    @app_name = 'Notebook'
+    render 'index'
   end
 end
